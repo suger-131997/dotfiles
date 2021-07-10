fc3322b6a680a05b1db21949c5cb7856c8b3bd82
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2021-07-11 04:34:46 +0900

    原因調査

--- .bin/zshinstall.sh
+++ .bin/zshinstall.sh
@@ -4,6 +4,6 @@ set -ue
 command mkdir ~/.zsh
 command curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 command curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-command curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+# command curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 
 command touch /root/bar
